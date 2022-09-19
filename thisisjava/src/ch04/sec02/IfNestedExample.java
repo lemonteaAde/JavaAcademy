6567c7f51d1f82aa49aefad00e663436dcaa5a73
admin <admin@PC17>
2022-09-19 15:50:16 +0900

    JAVA Academy(5)

---
+++ thisisjava/src/ch04/sec02/IfNestedExample.java
@@ -0,0 +1,30 @@
+package ch04.sec02;
+
+public class IfNestedExample {
+	public static void main(String[] args) {
+		int score = (int)(Math.random()*20) + 81; // 0~19, 80~100
+		System.out.println("점수: " + score);
+		
+		String grade;
+		
+		if(score>=90) { //90~100사이의 값 비교
+			if(score>=95) {
+				grade = "A+";
+			} else if(score<95) {
+				grade = "A-";
+			} else {
+				grade = "A0";
+			}
+		} else { //90미만인 경우	
+			if(score>=85) {
+				grade = "B+";
+			} else if(score<85) {
+				grade = "B-";
+			} else {
+				grade ="B0";
+			}
+		}
+		
+		System.out.println("학점: " + grade);
+	}
+}
