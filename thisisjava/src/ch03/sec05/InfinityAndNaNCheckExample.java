cfe599aa8b92f428551d4a259f54af1070688778
admin <admin@PC17>
2022-09-19 10:44:43 +0900

    collect(1)

--- thisisjava/src/ch03/sec05/InfinityAndNaNCheckExample.java
+++ thisisjava/src/ch03/sec05/InfinityAndNaNCheckExample.java
@@ -2,10 +2,11 @@ package ch03.sec05;
 
 public class InfinityAndNaNCheckExample {
 	public static void main(String[] args) {
+		//나눔을 당하는 수가 정수이고 나누는 수가 0.0일때 Infinity, NaN
 		int x = 5;
 		double y = 0.0;
 		double z = x / y;
-		//double z = x % y;
+		//double z = x % y;  // NaN (Not a Number)
 		
 		//잘못된 코드
 		System.out.println(z + 2);	
