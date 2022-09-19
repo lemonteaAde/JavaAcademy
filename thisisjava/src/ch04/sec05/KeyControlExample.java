cff2681f65cb29e96b285f8ca3651236f1d0b06d
admin <admin@PC17>
2022-09-19 17:10:51 +0900

    JAVA Academy(220919_6)

--- thisisjava/src/ch04/sec05/KeyControlExample.java
+++ thisisjava/src/ch04/sec05/KeyControlExample.java
@@ -13,18 +13,20 @@ public class KeyControlExample {
 			System.out.println("1. 증속 | 2. 감속 | 3. 중지");
 			System.out.println("-----------------------------");
 			System.out.print("선택: ");
-
+			//키보드로 부터 값 입력받기
 			String strNum = scanner.nextLine();
 
-			if(strNum.equals("1")) {
+			if(strNum.equals("1")) { // if("1".equals(strNum)) 이랑 같다
 				speed++;
 				System.out.println("현재 속도 = " + speed);
 			} else if(strNum.equals("2")) {
-				speed--;
+				if(speed--==0) speed=0;
 				System.out.println("현재 속도 = " + speed);
-			} else if(strNum.equals("3")) {
-				run = false;
+			} else if(strNum.equals("3")) { //while문은 반복문을 빠져나가는 조건이 블럭 내에 존재해야함.
+				run =!run; //run = false; 와 같다.
 			}
+			
+			System.out.println("현재 속도 = "+speed);
 		}
 
 		System.out.println("프로그램 종료");
