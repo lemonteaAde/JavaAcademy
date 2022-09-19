ddfd3262f8627f7f560c58daa0f31ea490821498
admin <admin@PC17>
2022-09-19 14:37:23 +0900

    java(4)

--- thisisjava/src/ch03/sec10/AssignmentOperatorExample.java
+++ thisisjava/src/ch03/sec10/AssignmentOperatorExample.java
@@ -2,7 +2,8 @@ package ch03.sec10;
 
 public class AssignmentOperatorExample {
 	public static void main(String[] args) {
-		int result = 0;		
+		int result = 0;
+		//복합 대입 연산
 		result += 10; // result= result + 10;
 		System.out.println("result=" + result);		
 		result -= 5;  // result = result -5;
